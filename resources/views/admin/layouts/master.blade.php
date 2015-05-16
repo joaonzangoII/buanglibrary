0c7ea46f027f1e6f82f36dd26c15ccd18b54eab1
Joao Nzango <joaonzangoii@hotmail.com>
2015-05-16 10:13:53 +0200

    cart

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -70,46 +70,7 @@
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
 					  <li><a href="{{ url('/') }}">Website</a></li>
-					  @if(!Auth::User()->isAdmin())
-					  <li><a  href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="1000" role="button" >{{ $cart->count() }} items <span class="glyphicon glyphicon-shopping-cart"></span><b class="caret"></b></a>
-						  <ul class="dropdown-menu" role="menu">
-						  <table class="table table-striped">
-						    <th>ID</th>
-						    <th>Title</th>
-						    <th>Quantity</th>
-						    <th>Price</th>
-						    <th>User</th>
-						    @if($cart->count() > 5)
-						    	@foreach($cart->items()->take(5) as $item)
-							      <tr>      
-								      <td>{{ $item->id }}</td>
-								      <td>{{ $item->name }}</td>
-								      <td>{{ $item->quantity }}</td>
-								      <td>{{ $item->price }}</td>
-								      <td>{{ \App\User::find($item->user_id)->fullname }}</td>
-							      </tr>
-						       @endforeach
-                @else
-                  @foreach($cart->items() as $item)
-							      <tr>      
-								      <td>{{ $item->id }}</td>
-								      <td>{{ $item->name }}</td>
-								      <td>{{ $item->quantity }}</td>
-								      <td>{{ $item->price }}</td>
-								      <td>{{ \App\User::find($item->user_id)->fullname }}</td>
-							      </tr>
-						       @endforeach
-						    @endif
-						      
-						  </table>
-				{{-- 		  
-						  @foreach ($cart->items() as $item)
-						  	<li><a tabindex="-1" href="#">{{ $item->name }}  - {{ $item->quantity }} </a></li>
-						  @endforeach --}}
-						  	<li><a class="text-center" href="{{ route("admin.bookings.cart") }}">View All</a></li>
-						  </ul>
-					  </li>
-					  @endif
+					  
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->fullname }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
